0f49e1fd78bd750782e2ebb23c87c06ee53485fc
purnatummala <151711173+purnatummala@users.noreply.github.com>
2024-03-04 21:42:51 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -98,7 +98,7 @@ def question3():
     answers = {}
 
     # type: a string that evaluates to a float
-    answers["(a) SSE"] = 4*R*R
+    answers["(a) SSE"] = '4*R*R'
 
     # type: a string that evaluates to a float
     answers["(b) SSE"] = 7.5
